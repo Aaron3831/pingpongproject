f0cbff87245d50a9d286eca4a16006ffdc183155
Aaron Nguyen <aaron3831@gmail.com>
2017-03-31 09:18:30 -0700

    add a range function

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,24 @@ $(document).ready(function() {
     event.preventDefault();
 
   var numberInput = parseInt($("input#number").val());
-    
+
+  function range0 (n) {
+    // create a array, from 0 to n, including n
+    var m = n+1;
+    var arr = Array(m);
+    for (var ii = 0; ii < m; ii++) { arr[ii] = ii; };
+    return arr;
   }
+  console.log( range0(10) ); // [ 0, 1, 2, 3]
+  //
+  // var outputPing = function(numberInput) {
+  //   if (numberInput % 3 === 0)
+  //     else if (math.range(0, numberInput, true);
+  //
+  // }
+  // var outputPong = function(numberInput) {
+  //   if (numberInput % 5 === 0)
+  //
+  // }
+  // for (var i = 0; index < numberInput; index +=1;)
 });
