a77c352f8820ac4869b5b31188cd65fb65fc1ce5
Aaron Nguyen <aaron3831@gmail.com>
2017-03-31 15:17:24 -0700

    add for loop to replace 3,5 and 15 with ping, pong and ping-pong respectively

--- js/scripts.js
+++ js/scripts.js
@@ -7,11 +7,26 @@ $(document).ready(function() {
 console.log(start)
 
   var result = [];
-  for (var i = 0; i<start+1; ++i) result.push(i)
+  for (var i = 1; i<start+1; ++i) result.push(i)
   console.log(result);
-    var result2 = result;
-    for (var j = 0; j < result.length; j++) {
-      result2.splice(j+3);
+    var result2 = result
+
+    var nth = 3;
+    var replaceWith = "ping"
+    for (var j = nth-1; j < result2.length-1; j+=nth) {
+    	result2[j] = replaceWith;
+    }
+
+    var nth = 5;
+    var replaceWith = "pong"
+    for (var k = nth-1; k < result2.length-1; k+=nth) {
+      result2[k] = replaceWith;
+    }
+
+    var mth = 15;
+    var replaceWith = "ping-pong"
+    for (var m = mth-1; m < result2.length-1; m+=mth) {
+      result2[m] = replaceWith;
     }
 
   $('#result').text(result2)
