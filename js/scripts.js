09d8dbe42e9f1d23b5b748e174b40fee93c16d78
Aaron Nguyen <aaron3831@gmail.com>
2017-03-31 15:21:33 -0700

    finalize for loop to include the last character on the 15 loop

--- js/scripts.js
+++ js/scripts.js
@@ -7,25 +7,25 @@ $(document).ready(function() {
 console.log(start)
 
   var result = [];
-  for (var i = 1; i<start+1; ++i) result.push(i)
+  for (var i = 0; i<start+1; ++i) result.push(i)
   console.log(result);
     var result2 = result
 
     var nth = 3;
     var replaceWith = "ping"
-    for (var j = nth-1; j < result2.length-1; j+=nth) {
+    for (var j = nth; j < result2.length-1; j+=nth) {
     	result2[j] = replaceWith;
     }
 
     var nth = 5;
     var replaceWith = "pong"
-    for (var k = nth-1; k < result2.length-1; k+=nth) {
+    for (var k = nth; k < result2.length-1; k+=nth) {
       result2[k] = replaceWith;
     }
 
     var mth = 15;
     var replaceWith = "ping-pong"
-    for (var m = mth-1; m < result2.length-1; m+=mth) {
+    for (var m = mth; m < result2.length; m+=mth) {
       result2[m] = replaceWith;
     }
 
