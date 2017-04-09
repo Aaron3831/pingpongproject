1cd99fe0a890022e87486361eb2e94e35e9545ac
Aaron3831 <aaron3831@gmail.com>
2017-04-09 16:11:38 -0700

    refactor code to properly separate business and ui

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,64 @@
+//business end
+
+var result = [];
+
+function divisibleBy(start) {
+  if (start % 15 === 0) {
+    return "ping-pong";
+  } else if (start % 5 === 0) {
+    return "pong";
+  } else if (start % 3 === 0) {
+    return "ping";
+  } else {
+    return start;
+  }
+}
+
+function arrayList(start) {
+  for (var i=1; i<=start; ++i) {
+    result.push(divisibleBy(i));
+  }
+}
+
 $(document).ready(function() {
   $("form#pingpong").submit(function(event) {
       event.preventDefault();
 
   var start = parseInt($("input#start").val());
+  console.log(start);
+  result = [];
 
-console.log(start)
-
-  var result = [];
-  for (var i = 0; i<start+1; ++i) result.push(i)
   console.log(result);
-  var result2 = result
 
-  var nth = 3;
-  var replaceWith = "ping"
-  for (var j = nth; j < result2.length-1; j+=nth) {
-  	result2[j] = replaceWith;
-  }
+  arrayList(start);
 
-  var nth = 5;
-  var replaceWith = "pong"
-  for (var k = nth; k < result2.length-1; k+=nth) {
-    result2[k] = replaceWith;
-  }
 
-  var nth = 15;
-  var replaceWith = "ping-pong"
-  for (var m = nth; m < result2.length; m+=nth) {
-    result2[m] = replaceWith;
-  }
+  // works but very messy with business and ui separation
 
-  $('#result').text(result2)
+  // var result = [];
+  // for (var i = 0; i<start+1; ++i) result.push(i)
+  // console.log(result);
+  // var result2 = result
+  //
+  // var nth = 3;
+  // var replaceWith = "ping"
+  // for (var j = nth; j < result2.length-1; j+=nth) {
+  // 	result2[j] = replaceWith;
+  // }
+  //
+  // var fifth = 5;
+  // var replaceWith = "pong"
+  // for (var k = fifth; k < result2.length-1; k+=fifth) {
+  //   result2[k] = replaceWith;
+  // }
+  //
+  // var fifteenth = 15;
+  // var replaceWith = "ping-pong"
+  // for (var m = fifteenth; m < result2.length; m+=fifteenth) {
+  //   result2[m] = replaceWith;
+  // }
+
+  $('#result').text(result)
 
     // function range (start, end) {
     //
